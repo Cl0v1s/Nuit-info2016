41fc37eb33ed2fec6216730107e02cfaf0da04ea
Sylvain Metayer <sylvain_metayer@outlook.fr>
2016-12-02 06:25:45 +0100

    Remove & add All cards

--- Src/Back/getCards.php
+++ Src/Back/getCards.php
@@ -8,12 +8,9 @@ $pdo = getConnection();
 $startId = $_GET["startId"];
 $length = $_GET["length"];
 
-$sql = "select * from Card where id_card>=:startId and id_card<:startId+:length";
+$sql = "select * from Card";
 
 $query = $pdo->prepare($sql);
-$query->bindValue (':startId', $startId);
-$query->bindValue(":length", $length);
-
 $ret = $query->execute();
 
 $result = array(
