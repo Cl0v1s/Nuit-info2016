3c767f835e36dcf147fac40c1b00c507627c0fb4
Antoine Barriere <antoine.barriere@etu.u-bordeaux.fr>
2016-12-02 04:20:33 +0100

    GetUser.php fixed

--- Src/Back/getUser.php
+++ Src/Back/getUser.php
@@ -1,21 +1,24 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+
 session_start();
 
 require_once("functions.php");
 $pdo = getConnection();
 
-$userId = $_GET["userId"];
-
-$sql = "SELECT username,mail,priority,date_creation,id_user FROM User where id=:userId";
+$userId = $_GET["id_user"];
+$sql = "SELECT username,mail,priority,date_creation,id_user FROM Users where id_user=:userId";
 $query = $pdo->prepare($sql);
-$query->bindValue (':userId', $userId);
+//$query->bindValue (':userId', $userId);
 
-$row = $query->query()->fetch();
 
+
+//$row = $query->query()->fetch();
+$query->execute(array('userId'=>$userId));
+$row=$query->fetch();
 $result = array(
 "code" => "",
 "content" => "");
+
 
 if (empty($row)) {
     $result["code"] = 500;
@@ -24,13 +27,14 @@ if (empty($row)) {
     exit(1);
 }
 
+
 $result["code"] = 200;
 $result["content"] = array(
-    "id_user" => $row->id_user, 
-    "username" => $row->username,
-    "mail" => $row->mail,
-    "date_creation" => $row->date,
-    "priority" => $row->priority
+    "id_user" => $row['id_user'], 
+    "username" => $row['username'],
+    "mail" => $row['mail'],
+    "date_creation" => $row['date'],
+    "priority" => $row['priority']
 );
 
 echo json_encode($result);
