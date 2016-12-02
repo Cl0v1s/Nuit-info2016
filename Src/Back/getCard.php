68d0c58e475de1453d4adf3ae1f7aa58834eba24
Sylvain Metayer <sylvain_metayer@outlook.fr>
2016-12-02 03:41:10 +0100

    Fix Card & Sticker

--- Src/Back/getCard.php
+++ Src/Back/getCard.php
@@ -6,17 +6,17 @@ $pdo = getConnection();
 
 $cardId = $_GET["cardId"];
 
-$sql = "select * from Card where id=:cardId";
+$sql = "select * from Card where id_card=:cardId";
 $query = $pdo->prepare($sql);
 $query->bindValue (':cardId', $cardId);
-
-$row = $query->query()->fetch();
+$ret = $query->execute();
+$row = $query->fetch();
 
 $result = array(
 "code" => "",
 "content" => "");
 
-if (empty($row)) {
+if ($row == false) {
     $result["code"] = 500;
     $result["content"] = "Error while retrieving a card";
     echo json_encode($result);
@@ -25,11 +25,11 @@ if (empty($row)) {
 
 $result["code"] = 200;
 $result["content"] = array(
-"id_card" => $row->id_card,
-"description" => $row->description,
-"title" => $row->title,
-"date" => $row->date,
-"priority" => $row->priority
+"id_card" => $row["id_card"],
+"description" => $row["description"],
+"title" => $row["title"],
+"date" => $row["date"],
+"priority" => $row["priority"]
 );
 
 echo json_encode($result);
--- Src/Back/getSticker.php
+++ Src/Back/getSticker.php
@@ -7,17 +7,18 @@ $pdo = getConnection();
 
 $sticker_id = $_GET["stickerId"];
 
-$sql = "SELECT * FROM Sticker where id=:sticker_id";
+$sql = "SELECT * FROM Sticker where id_sticker=:sticker_id";
 $query = $pdo->prepare($sql);
 $query->bindValue (':sticker_id', $sticker_id);
 
-$row = $query->query()->fetch();
+$ret = $query->execute();
+$row = $query->fetch();
 
 $result = array(
 "code" => "",
 "content" => "");
 
-if (empty($row)) {
+if ($row == false) {
     $result["code"] = 500;
     $result["content"] = "Error while retrieving sticker";
     echo json_encode($result);
@@ -26,11 +27,11 @@ if (empty($row)) {
 
 $result["code"] = 200;
 $result["content"] = array(
-    "id_sticker" => $row->id_sticker, 
-    "title" => $row->title,
-    "logo" => $row->logo,
-    "description" => $row->description,
-    "cond" => $row->cond
+"id_sticker" => $row["id_sticker"],
+"title" => $row["title"],
+"logo" => $row["logo"],
+"description" => $row["description"],
+"cond" => $row["cond"]
 );
 
 echo json_encode($result);
